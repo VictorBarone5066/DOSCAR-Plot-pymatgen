b22f5548600f8aea3db902c437f099d3cf1f9a3b
VictorBarone5066 <52221953+VictorBarone5066@users.noreply.github.com>
2020-06-22 21:30:21 -0400

    Add files via upload

--- DosPlotterExample.py
+++ DosPlotterExample.py
@@ -11,24 +11,27 @@ plt.rcParams["mathtext.fontset"] = "dejavuserif"
 plt.tight_layout = True
 
 #Initialize a vasprun instance and a myClass instance
-v = Vasprun('vasprun.xml.AgBiI4')
+v = Vasprun("C://Users//baron//Desktop//xmlgen//spdDosPol//vasprun.xml")
 test = myClass.TempName(v)
 
-#Add elements to the plot.  In this case, add all of them.  Sigma = (cubic) spline smoothing
-for elem in test.uniqueElements:
-    test.GetElementDosPlot(elem, sigma = 0.065)
+if(test.IsLDecomposed(v)):  
+    #Add elements to the plot.  In this case, add all of them.  Sigma = (cubic) spline smoothing
+    for elem in test.uniqueElements:
+        test.GetElementDosPlot(elem, sigma = 0.065)
     
-#Customize the plot with whatever else you want    
-plt.title("hello :)")
-plt.xlabel(r"$E-E_\mathrm{F}$ (eV)")
-plt.ylabel("DOS (states / eV)")
-plt.legend(handles = test.legend)
-plt.ylim(0)
-plt.xlim(-5, 0)
-
-#Be cure to save (if necessary) and clear the plot when done
-plt.savefig("exmple.pdf")
-plt.clf()
+  
+    #Customize the plot with whatever else you want    
+    plt.title("hello :)")
+    plt.xlabel(r"$E-E_\mathrm{F}$ (eV)")
+    plt.ylabel("DOS (states / eV)")
+    #plt.legend(handles = test.legend)
+    test.GenerateLegend()
+    if(not test.spinPol):
+        plt.ylim(0)
+    
+    #Be cure to save (if necessary) and clear the plot when done
+    plt.savefig("C://Users//baron//Desktop//exmple.pdf")
+    plt.clf()
 
   
     
--- class_.py
+++ class_.py
@@ -20,9 +20,9 @@ def GetColor(i):
 
 
 class TempName:
-    plot = None
     completeDos = None
     uniqueElements = []    
+    spinPol = None
     
     lineStyle = {'s': '-',
                  'p': '--',
@@ -31,30 +31,41 @@ class TempName:
     legend = []
     
     def __init__(self, vasprun):
-        #Initialize list of unique elements and their pdos
-        for i in range(0, len(vasprun.complete_dos.structure.sites)):
-            counted = False
-            for j in range(0, len(self.uniqueElements)):
-                if(vasprun.complete_dos.structure.sites[i].species_string == self.uniqueElements[j].name):
-                    counted = True
-                    break
-            if(not counted):
-                self.uniqueElements.append(Element(vasprun.complete_dos.structure.sites[i].species_string))
-        
-        self.completeDos = vasprun.complete_dos
-        
+        #Initialize list of unique elements and their pdos if formatting is correct
+        if(self.IsLDecomposed(vasprun)):
+            for i in range(0, len(vasprun.complete_dos.structure.sites)):
+                counted = False
+                for j in range(0, len(self.uniqueElements)):
+                    if(vasprun.complete_dos.structure.sites[i].species_string == self.uniqueElements[j].name):
+                        counted = True
+                        break
+                if(not counted):
+                    self.uniqueElements.append(Element(vasprun.complete_dos.structure.sites[i].species_string))
+            
+            self.completeDos = vasprun.complete_dos
+        else:
+            print("BadVasprunWarning:  The DOS info in your .xml file is not formatted into s-p-d components.")
+    
+        #Determine if spin pol is on
+        self.spinPol = self.IsSpinPolarized(vasprun)
+    
+    def IsLDecomposed(self, vasprun):
+        if(vasprun.pdos == []):
+            return False
+        return True
+    
+    def IsSpinPolarized(self, vasprun):
+        return vasprun.is_spin
+    
     def GetElementDosPlot(self, elements, sigma = 0, scaleByEf = True, hideS = False, hideP = False, hideD = False):
         global COLORS
         global COLOR_INDEX
-        COLOR_INDEX = (COLOR_INDEX + 1)%len(COLORS)
+        COLOR_INDEX = (COLOR_INDEX + 1)%len(COLORS) if (len(self.uniqueElements) > 1) else (COLOR_INDEX)%len(COLORS)
         color = tuple(GetColor(COLOR_INDEX))   
          
         if(not isinstance(elements, list)):
             elements = [elements]
         
-        #This is (probably) the non-general part.  I need info on what kinds of things get_element_spd_dos
-        #can return to make this work for all cases (xyz decomposed?  Where is the spin-pol stuff at???
-        #what happens if you just have total DOS w/o spd?).
         for element in elements:
             self.legend.append(Line2D([0], [0], color=color, lw=2, label=element.name))
             
@@ -68,13 +79,39 @@ class TempName:
                 style = self.lineStyle['s']
                 y = s.densities[Spin.up] if sigma <= 0 else s.get_smeared_densities(sigma)[Spin.up]
                 plt.plot(x, y, color=color, linestyle=str(style), linewidth=.8)
+                ##condition for spin pol
+                if(self.spinPol):
+                   y = [-y for y in s.densities[Spin.down]] if sigma <= 0 else [-y for y in s.get_smeared_densities(sigma)[Spin.down]]
+                   plt.plot(x, y, color=color, linestyle=str(style), linewidth=.8)                 
             if(not hideP):
                 style = self.lineStyle['p']
                 y = p.densities[Spin.up] if sigma <= 0 else p.get_smeared_densities(sigma)[Spin.up]                
                 plt.plot(x, y, color=color, linestyle=str(style), linewidth=.8)            
+                ##condition for spin pol
+                if(self.spinPol):
+                   y = [-y for y in p.densities[Spin.down]] if sigma <= 0 else [-y for y in p.get_smeared_densities(sigma)[Spin.down]]
+                   plt.plot(x, y, color=color, linestyle=str(style), linewidth=.8)
             if(not hideD):
                 style = self.lineStyle['d']
                 y = d.densities[Spin.up] if sigma <= 0 else d.get_smeared_densities(sigma)[Spin.up]                
                 plt.plot(x, y, color=color, linestyle=str(style), linewidth=.8)        
+                ##condition for spin pol
+                if(self.spinPol):
+                   y = [-y for y in d.densities[Spin.down]] if sigma <= 0 else [-y for y in d.get_smeared_densities(sigma)[Spin.down]]
+                   plt.plot(x, y, color=color, linestyle=str(style), linewidth=.8)
+        return
+    
+    def GenerateLegend(self, atoms = True, lineStyles = True):
+        if(atoms):
+            first = plt.legend(handles = self.legend, loc = "upper right")
+            plt.gca().add_artist(first)    
+        
+        if(lineStyles):
+            legend2 = []
+            legend2.append(Line2D([0], [0], color='k', lw=2, linestyle = self.lineStyle['s'], label='s'))
+            legend2.append(Line2D([0], [0], color='k', lw=2, linestyle = self.lineStyle['p'], label='p'))
+            legend2.append(Line2D([0], [0], color='k', lw=2, linestyle = self.lineStyle['d'], label='d'))
+            plt.legend(handles = legend2, loc = "lower right")
+        
         return
 
